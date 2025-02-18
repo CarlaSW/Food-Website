c89fcb86b23abf2f32f50cd83f903f32e85cdc4c
CarlaS <carlasamy200@gmail.com>
2025-02-18 14:12:22 +0200

    Initialize project and needed packages

---
+++ foodapp/src/App.jsx
@@ -0,0 +1,34 @@
+import { useState } from "react";
+import Search from "./components/Search";
+import FoodList from "./components/FoodList";
+import Nav from "./components/Nav";
+import "./app.css";
+import Container from "./components/Container";
+import InnerContainer from "./components/InnerContainer";
+import FoodDetails from "./components/FoodDetails";
+
+function App() {
+  const [foodData, setFoodData] = useState([]);
+  const [foodId, setFoodId] = useState("658615");
+
+  return (
+    <div className="App">
+      <Nav />
+      <Search foodData={foodData} setFoodData={setFoodData} />
+      <Container>
+        <InnerContainer>
+          <FoodList
+            foodData={foodData}
+            setFoodData={setFoodData}
+            setFoodId={setFoodId}
+          />
+        </InnerContainer>
+        <InnerContainer>
+          <FoodDetails foodId={foodId} setFoodId={setFoodId} />
+        </InnerContainer>
+      </Container>
+    </div>
+  );
+}
+
+export default App;
